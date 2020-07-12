73cf7b0b5fd220fee153ec3de25738ae89b7fb0f
happou31 <happo31@gmail.com>
2020-07-12 18:19:37 +0900

    fix: Tweetする文字列

--- src/front/containers/App/Header.tsx
+++ src/front/containers/App/Header.tsx
@@ -7,7 +7,7 @@ export default () => (
     <p
       dangerouslySetInnerHTML={{
         __html: `
-      <a href="https://twitter.com/intent/share?&ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-text="プログラミング言語『Pekolanguage』" data-url="https://happou31.github.io/pekolanguage/" data-hash-tag="Pekolanguage" data-related="usadapekora" data-show-count="false">Tweet #Pekolanguage</a>
+      <a href="https://twitter.com/intent/share?&ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-text="プログラミング言語『ぺこーら』 #Pekolanguage" data-url="https://happou31.github.io/pekolanguage/" data-hash-tag="Pekolanguage" data-related="usadapekora" data-show-count="false">Tweet #Pekolanguage</a>
     `,
       }}
     ></p>
